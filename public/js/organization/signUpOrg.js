a866cac3ea7a136ca9a9fed9380c912038c36428
JPPinto <jose.vc.pinto@gmail.com>
2014-10-11 18:27:53 +0100

    Deleted debugging code

--- public/js/organization/signUpOrg.js
+++ public/js/organization/signUpOrg.js
@@ -1,42 +1,4 @@
 /*Fields verifications for the register process using Bootstrap validator*/
-/*$( "#registerOrgform" ).submit( function(event){
-
-  var frm = $('#registerOrgform');
-  var frmdata = frm.serializeArray();
-  console.log(frmdata);
-
-  var temp_causes=[];
-  for(var i=0;i<frmdata.length;i++){
-    if(frmdata[i].name==="volunteeringInterestsParam"){
-      temp_causes.push(frmdata[i].value);
-      console.log(frmdata[i]);
-    }
-  }
-  console.log(temp_causes);
-  var account = {
-      name: frmdata[0].value,
-      email: frmdata[2].value,
-      website: frmdata[1].value,
-      password: frmdata[4].value,
-      address: {
-        address: frmdata[5].value,
-        municipaly: frmdata[6].value,
-        district: frmdata[7].value},
-      about: frmdata[8].value,
-      birthdate: frmdata[9].value,
-      causes: temp_causes,
-      role:"organization"
-  };
-
-  $.ajax({
-      url: '/register',
-      type: 'POST',
-      data: account,
-      datatype: 'json'
-  });
-  alert("done");
-  event.preventDefault();
-});*/
 
 $(document).ready(function() {
     $( "#registerOrgform").bootstrapValidator({
@@ -179,16 +141,13 @@ $(document).ready(function() {
             var $form = $(e.target);
             var account;
             var frmdata = $form.serializeArray();
-            console.log(frmdata);
 
             var temp_causes=[];
             for(var i=0;i<frmdata.length;i++){
               if(frmdata[i].name==="volunteeringInterestsParam"){
                 temp_causes.push(frmdata[i].value);
-                console.log(frmdata[i]);
               }
             }
-            console.log(temp_causes);
 
             account = {
                 name: frmdata[0].value,
