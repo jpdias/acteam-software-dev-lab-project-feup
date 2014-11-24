dc24c186ff9c4b10206f1e0eec8e0061c1e33ca4
Joao Pedro Dias <jpdias@live.com.pt>
2014-11-24 16:19:16 +0000

    dropdown stuff7

--- public/js/search.js
+++ public/js/search.js
@@ -1,24 +1,33 @@
-$(document).ready(function(){
-	$('dropdown').dropdown();
+$(document).ready(function() {
+  $(function() {
+
+    $(".dropdown-menu li a").click(function() {
+
+      $(".drop:first-child").text($(this).text());
+      $(".drop:first-child").val($(this).text());
+
+    });
+
+  });
 });
 
 $('#searchButton').on('click', function(e) {
-	$.ajax({
-        url: '/searchorg',
-        type: 'POST',
-        data: {
-			name: $('#searchText').val(),
-			region: $('#').val()
-			/*var num = null;
+  $.ajax({
+    url: '/searchorg',
+    type: 'POST',
+    data: {
+      name: $('#searchText').val(),
+      cause: $(".drop:first-child").val()
+        /*var num = null;
 			$(".btn-group-justified > .btn").on("click", function(){
 				num = +this.innerHTML;
 				alert("Value is " + num);
 			});*/
-        },
-        datatype: 'json',
-        success: function(message) {
-			
-          }
-      });
-    e.preventDefault();
+    },
+    datatype: 'json',
+    success: function(message) {
+
+    }
+  });
+  e.preventDefault();
 });
